8cb8019a2d8622612b922df1fbb4a2712d4cfdf5
Hansol Yun <solhanyun@gmail.com>
2022-04-24 04:59:11 +0900

    Chore initial prisma settings

---
+++ prisma/script.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function main() {
+  /**
+   * NOTE data가 생성될 때 deleted_at은 null입니다.
+   */
+  prisma.$use(async (params, next) => {
+    if (params.action === 'create') {
+      params.args.where.deleted_at = null;
+    }
+
+    return next(params);
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
