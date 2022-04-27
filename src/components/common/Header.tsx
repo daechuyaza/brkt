b93dba0369c879b8b15c6e971c4267b2923286e6
Juhyoung JUNG <juhyoungjung@Juhyoungs-MacBook-Air.local>
2022-04-27 23:31:57 +0900

    Feature add upper lower header layout

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,43 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <UpperWrapper>
+        <LogoBox />
+        <UpperLeftNavigatorBox />
+      </UpperWrapper>
+      <LowerWrapper />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 11.4rem;
+`;
+
+const UpperWrapper = styled.div`
+  display: flex;
+
+  flex: 2 1 0;
+  border-bottom: 1px solid black;
+`;
+
+const LogoBox = styled.div`
+  background-color: black;
+`;
+
+const UpperLeftNavigatorBox = styled.div`
+  width: 15%;
+  height: '100%';
+  background-color: black;
+`;
+
+const LowerWrapper = styled.div`
+  flex: 2 1 0;
+  border-bottom: 1px solid black;
+`;
---
+++ src/pages/main.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Header from '@components/common/Header';
+
+export default function Main() {
+  return <Header />;
+}
