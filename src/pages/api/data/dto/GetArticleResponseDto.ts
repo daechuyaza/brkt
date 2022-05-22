99c1cd0d30a7190b3212869f272fbc8dbb7ce507
hayley <hayley@soomgo.com>
2022-05-22 18:28:45 +0900

    [BE-0003] Feature add mapper, dto

---
+++ src/pages/api/data/dto/GetArticleResponseDto.ts
@@ -0,0 +1,18 @@
+import { Nullable } from '@common/utils';
+import { Article, Series, User } from '../model';
+
+export interface GetArticleResponseDto {
+    id: number,
+    authorId: number,
+    seriesId: Nullable<number>,
+    author: User,
+    series: Nullable<Series>
+    title: string,
+    content: string,
+    thumbnail: string,
+    viewCount: number,
+    isPublic: boolean;
+    createdAt: Date,
+    updatedAt: Date,
+    deletedAt: Nullable<Date>,
+};
