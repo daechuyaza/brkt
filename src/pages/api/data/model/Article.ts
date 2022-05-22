7071c67816da666824993742ccbe19a2d2882a67
hayley <hayley@soomgo.com>
2022-05-22 18:28:06 +0900

    [BE-0003] Feature add model

---
+++ src/pages/api/data/model/Article.ts
@@ -0,0 +1,16 @@
+import { Nullable } from "../../../../common/utils/TypeUtils";
+import { Series, User } from "./index";
+
+export interface Article {
+    id: number,
+    author: User,
+    series: Nullable<Series>
+    title: string,
+    content: string,
+    thumbnail: string,
+    viewCount: number,
+    isPublic: boolean;
+    createdAt: Date,
+    updatedAt: Date,
+    deletedAt: Nullable<Date>,
+}
