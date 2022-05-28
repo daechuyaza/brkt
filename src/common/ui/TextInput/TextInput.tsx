60e267005615db3ffc132a6ab02214b9c99d96a5
doheekim <doheekim@cclss.net>
2022-05-28 19:35:12 +0900

    Feature add global text input

--- src/common/ui/TextInput/TextInput.tsx
+++ src/common/ui/TextInput/TextInput.tsx
@@ -1,5 +1,106 @@
-import React from 'react';
+import { css, SerializedStyles, Theme } from '@emotion/react';
+import styled from '@emotion/styled';
+import { InputHTMLAttributes } from 'react';
+import { Path, RegisterOptions, useFormContext } from 'react-hook-form';
 
-export function TextInput() {
-  return <div>TextInput</div>;
+type IFormValue = {
+  [key: string]: string;
+};
+
+type Props = {
+  defaultText: string;
+  disabled?: boolean;
+  name: Path<IFormValue>;
+  placeholder?: string;
+  prefix?: React.ReactNode;
+  size?: 'medium' | 'large';
+  surfix?: React.ReactNode;
+  type?: InputHTMLAttributes<HTMLInputElement>['type'];
+  validation?: RegisterOptions;
+};
+
+type SizeTypes = Record<string, (props: Theme) => SerializedStyles>;
+
+export function TextInput({
+  defaultText,
+  disabled = false,
+  name,
+  placeholder,
+  prefix,
+  size = 'medium',
+  surfix,
+  type = 'text',
+  validation
+}: Props) {
+  const {
+    register,
+    formState: { errors }
+  } = useFormContext();
+
+  return (
+    <>
+      <InputWrapper>
+        {prefix}
+        <Input
+          {...register(name, validation)}
+          disabled={disabled}
+          $size={size}
+          placeholder={placeholder}
+          type={type}
+          defaultValue={defaultText}
+        />
+        {surfix}
+      </InputWrapper>
+      {errors[name] && <ErrorMessage>{errors[name].message}</ErrorMessage>}
+    </>
+  );
 }
+
+const sizes: SizeTypes = {
+  large: (theme: Theme) => css`
+    font-size: ${theme.fontSize.subtitle};
+    font-weight: bold;
+
+    &::placeholder {
+      color: ${theme.colors.secondary};
+      font-size: ${theme.fontSize.subtitle};
+    }
+  `,
+  medium: (theme: Theme) => css`
+    font-size: ${theme.fontSize.caption1};
+    font-weight: normal;
+
+    &::placeholder {
+      color: ${theme.colors.secondary};
+      font-size: ${theme.fontSize.caption1};
+    }
+  `
+};
+
+const Input = styled.input<{
+  $size: 'medium' | 'large';
+}>`
+  background: transparent;
+  border: none;
+  display: block;
+  flex: 1;
+  outline: none;
+
+  ${(props) => sizes[props.$size](props.theme)};
+`;
+
+const InputWrapper = styled.div`
+  align-items: center;
+  border-bottom: ${(props) => `1px solid ${props.theme.colors.onBackground}`};
+  display: flex;
+  padding-bottom: ${(props) => props.theme.spacing[2]};
+  padding-top: ${(props) => props.theme.spacing[2]};
+  width: 100%;
+`;
+
+const ErrorMessage = styled.span`
+  color: ${(props) => props.theme.colors.error};
+  display: block;
+  font-size: ${(props) => props.theme.fontSize.caption2};
+  margin-top: ${(props) => props.theme.spacing[0]};
+`;
