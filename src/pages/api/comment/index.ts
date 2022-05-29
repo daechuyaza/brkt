5bd8fdded78d6982d205be06f3be6cfa52d5c283
Hansol Yun <solhanyun@gmail.com>
2022-05-29 21:04:37 +0900

    [BE-0010] Feature add comment post handler

---
+++ src/pages/api/comment/index.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+import type { NextApiResponse } from 'next';
+import type { CreateCommentDto } from '../data/dto/CreateCommentDto';
+
+export default async function handler(req: CreateCommentDto, res: NextApiResponse) {
+  const prisma = new PrismaClient({ log: ['query'] });
+  const { articleId, userId, commentText } = req.body;
+
+  try {
+    const article = await prisma.post.findUnique({
+      where: {
+        id: articleId
+      }
+    });
+
+    if (!article) {
+      // TODO error handling
+      throw new Error(`Article not found where id: ${articleId}`);
+    }
+
+    const comment = await prisma.comment.create({
+      data: {
+        userId,
+        postId: articleId,
+        comment: commentText
+      }
+    });
+
+    res.status(200).json(comment);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
