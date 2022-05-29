ee68e1cd87e1c0c35cb897adfd76cbfe68c7c6ec
Hansol Yun <solhanyun@gmail.com>
2022-05-29 21:04:55 +0900

    [BE-0010] add CommentRepository

---
+++ src/pages/api/repository/CommentRepository.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { CreateCommentDto } from '../data/dto/CreateCommentDto';
+import { MessageDto } from '../data/dto/MessageDto';
+
+export class CommentRepository {
+  createComment = async (req: CreateCommentDto) => {
+    const { body } = req;
+    const response = await axios.post('http://localhost:3000/api/comment', body);
+
+    if (response.status === 200) {
+      return new MessageDto('Successfully created comment');
+    }
+  };
+}
