1513655b7c8ee5793956afd0041eb8e3a9127f2b
Juhyoung JUNG <juhyoung.jung1992@gmail.com>
2022-07-03 21:50:49 +0900

    Fix right arrow icon size

--- src/common/assets/icons/ArrowRight.tsx
+++ src/common/assets/icons/ArrowRight.tsx
@@ -2,9 +2,15 @@ import * as React from 'react';
 import { SVGProps } from 'react';
 
 export const ArrowRight = (props: SVGProps<SVGSVGElement>) => (
-  <svg width={'32px'} height={'64px'} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+  <svg
+    width={27}
+    height={48}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
     <path
-      d="m37 37 2.004-2.004L41.007 37l-2.003 2.003L37 37ZM5.004.996l34 34-4.008 4.007-34-34L5.005.996Zm34 38.007-34 34-4.007-4.007 34-34 4.007 4.007Z"
+      d="m24 24 1.296-1.296L26.593 24l-1.297 1.296L24 24ZM3.296.704l22 22-2.592 2.592-22-22L3.296.704Zm22 24.592-22 22-2.592-2.592 22-22 2.592 2.592Z"
       fill="#232323"
     />
   </svg>
