5c308b974cce06639f085b47dbc8033431780b9e
Juhyoung JUNG <juhyoung.jung1992@gmail.com>
2022-08-07 13:10:27 +0900

    Chore test simple git hook#1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,8 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
   require('../../mocks');
 }
 
+console.log('hi');
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={ReactQueryUtils.getQueryClient()}>
